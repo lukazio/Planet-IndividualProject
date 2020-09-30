e986552002e24b9857cfc9a965e1bd9d5ece9ef4
LUKAZ <lukazlim@yahoo.com>
2020-09-30 19:16:28 +0800

    Finished about page

--- about.php
+++ about.php
@@ -18,6 +18,13 @@
     ?>
     
     <body style="margin-top:80px;" class="mx-3 mx-lg-5">
-        
+        <h3 class="text-info">
+            Made by Lukaz Lim Wei Hwang<br>
+            303COM Individual Project<br>
+            IICP: P17009036<br>
+            CU: 9856242<br>
+            <br>
+            GitHub repo: <a href="https://github.com/lukazio/Planet-IndividualProject">https://github.com/lukazio/Planet-IndividualProject</a>
+        </h3>
     </body>
 </html>
